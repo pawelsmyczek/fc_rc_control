1f79b765182125660d5959a96a59fa13242ad4db
Pawel Smyczek <pawel.smyczek1@gmail.com>
2024-06-30 23:36:25 +0200

    1. Rework handling files opening. 2. Rewok handling input from paddev, outputting to serial and vice versa 3. Add requirements file.

--- rc_command.py
+++ rc_command.py
@@ -7,6 +7,10 @@ import struct
 import time
 import tty
 import serial
+from scipy.interpolate import interp1d
+from scipy.signal import iirfilter, lfilter
+from collections import deque
+import numpy as np
 
 if os.uname().nodename == 'raspberrypi':
     import RPi.GPIO as GPIO
@@ -14,6 +18,7 @@ if os.uname().nodename == 'raspberrypi':
 # from serial.tools import list_ports
 # from pynput import keyboard
 from threading import Thread
+from threading import Lock
 from evdev import InputDevice, categorize, ecodes
 import select
 
@@ -102,15 +107,21 @@ class RX_FRAME:
                                                                                          self.height)
 
 
+finish_tx = False
+finish_rx = False
+
+
 class E32:
     def __init__(self, p, baud):
         self.baud = baud
-        self.dev = serial.Serial(port=p, baudrate=baud, parity=serial.PARITY_NONE, timeout=0.1, write_timeout=0.1)
-        self.read_thread = Thread(target=self.write_, args=())
+        self.dev = serial.Serial(port=p, baudrate=baud, parity=serial.PARITY_NONE, timeout=0.09, write_timeout=0.03)
+        self.write_thread = Thread(target=self.write_, args=())
+        self.read_thread = Thread(target=self.read_, args=())
         self.buffer = None
         self.write_buffer = []
         self.hb = 0
         self.MAGIC_TX = 12345
+        self.serial_lock = Lock()
         self.finish_all = False
 
     def write(self, data):
@@ -176,103 +187,209 @@ class E32:
         time.sleep(0.1)
         self.dev.baudrate = self.baud
 
-    def write_(self):
-        print("starting to read/write")
+    def read_(self):
+        print("starting to read from rx/tx")
+        global finish_rx
         last_update_rx_frame = 0
-        rx_time_update = 0.8
-        while not self.finish_all:
+        rx_time_update = 0.9
+        while finish_rx == False:
             time.sleep(0.008)
             now = time.time()
             try:
+                if now >= last_update_rx_frame:
+                    print("reading")
+                    last_update_rx_frame = now + rx_time_update
+                    with self.serial_lock:
+                        self.buffer = self.dev.read(sys.getsizeof(RX_FRAME) + 2)
+                    if len(self.buffer) < 34:
+                        continue
+                    for i in range(0, len(self.buffer)):
+                        if self.buffer[i] != 60:
+                            continue
+                        print("buff[{}]={}".format(i, self.buffer[0]))
+                        if len(self.buffer) < i + 33 + 1:
+                            continue
+                        if self.buffer[i + 33] == 62:
+                            rx_frame = RX_FRAME(self.buffer[i:i + 34])
+                            print("read frame {}".format(str(rx_frame)))
+            except serial.SerialException as eser:
+                print("serial error on read: {}".format(eser.strerror))
+                break
+            except TypeError:
+                continue
+        time.sleep(1)
+        print("Finished reading rxtx")
+
+    def write_(self):
+        global finish_tx
+        global RPYT
+        print("starting to write to rx/tx")
+        while finish_tx == False:
+            time.sleep(0.008)
+            try:
                 self.write_buffer.clear()
-                self.hb += 1
-                if self.hb >= 16000:
-                    self.hb = 0
+
+                self.hb = self.hb + 1 if self.hb < 16375 else 0
+
                 txframe = TX_FRAME(self.MAGIC_TX, self.hb
                                    , RPYT["ROLL"], RPYT["PITCH"], RPYT["YAW"], RPYT["THROTTLE"], State)
-
-                self.write(bytes(txframe))
+                with self.serial_lock:
+                    self.write(bytes(txframe))
             except serial.SerialException as eser:
-                print(eser.strerror)
+                print("serial error on write: {}".format(eser.strerror))
+                break
             except TypeError:
-                print("type error")
-            if now >= last_update_rx_frame:
-                last_update_rx_frame = now + rx_time_update
-                self.read()
-                # if len(self.buffer) >= 34:
-                #     for i in range(0, len(self.buffer)):
-                #         if self.buffer[i] == 60:
-                #             print("buff[{}]={}".format(i, self.buffer[0]))
-                #             if len(self.buffer) >= i + 33 + 1:
-                #                 if self.buffer[i + 33] == 62:
-                #                     rx_frame = RX_FRAME(self.buffer[i:i + 34])
-                #                     print("read frame {}".format(str(rx_frame)))
-                if len(self.buffer) < 34:
-                    continue
-                for i in range(0, len(self.buffer)):
-                    if self.buffer[i] != 60:
-                        continue
-                    print("buff[{}]={}".format(i, self.buffer[0]))
-                    if len(self.buffer) < i + 33 + 1:
-                        continue
-                    if self.buffer[i + 33] == 62:
-                        rx_frame = RX_FRAME(self.buffer[i:i + 34])
-                        print("read frame {}".format(str(rx_frame)))
+                continue
+        time.sleep(1)
+        print("Finished writing to rxtx")
 
     def finish(self):
+        global finish_tx
+        global finish_rx
+        finish_tx = True
+        finish_rx = True
+        self.read_thread.join(2)
+        self.write_thread.join(2)
         self.dev.close()
-        self.finish_all = True
-        self.read_thread.join()
+        print("e32.finish()")
 
     def start_reading(self):
         self.read_thread.start()
+        self.write_thread.start()
 
+
+finish_pad = False
+
+from collections import deque
+
+class LiveFilter:
+    """Base class for live filters.
+    """
+    def process(self, x):
+        # do not process NaNs
+        if np.isnan(x):
+            return x
+
+        return self._process(x)
+
+    def __call__(self, x):
+        return self.process(x)
+
+    def _process(self, x):
+        raise NotImplementedError("Derived class must implement _process")
+class LiveLFilter(LiveFilter):
+    def __init__(self, b, a):
+        """Initialize live filter based on difference equation.
+
+        Args:
+            b (array-like): numerator coefficients obtained from scipy.
+            a (array-like): denominator coefficients obtained from scipy.
+        """
+        self.b = b
+        self.a = a
+        self._xs = deque([0] * len(b), maxlen=len(b))
+        self._ys = deque([0] * (len(a) - 1), maxlen=len(a)-1)
+
+    def _process(self, x):
+        """Filter incoming data with standard difference equations.
+        """
+        self._xs.appendleft(x)
+        y = np.dot(self.b, self._xs) - np.dot(self.a[1:], self._ys)
+        y = y / self.a[0]
+        self._ys.appendleft(y)
+
+        return y
 
 class PadDev:
 
     def __init__(self, name):
         self.pad = InputDevice(name)
         self.read_thread = Thread(target=self.read, args=())
-        self.finish = False
         print("Device:\n {}\n\n Its capabilites:\n {}".format(self.pad, self.pad.capabilities(verbose=True)))
+        self.xmap11 = interp1d([-32768, 32767], [-1, 1])
+        self.xremap = interp1d([-1, 1], [-1000, 1000])
+        bfilt, afilt = iirfilter(4, Wn=2.5, fs=30, btype="low", ftype="butter")
+        self.livefilter = LiveLFilter(bfilt, afilt)
 
     def start(self):
         self.read_thread.start()
 
+    def MAP(self, value, leftMin, leftMax, rightMin, rightMax):
+        # Figure out how 'wide' each range is
+        leftSpan = leftMax - leftMin
+        rightSpan = rightMax - rightMin
+
+        # Convert the left range into a 0-1 range (float)
+        valueScaled = float(value - leftMin) / float(leftSpan)
+
+        # Convert the 0-1 range into a value in the right range.
+        return rightMin + (valueScaled * rightSpan)
+    def apply_dummy_expo(self, x, expo, out_left, out_right):
+        # return x
+        # stupid but works, applies lower values at around 0
+        # TODO still flickers at some values, maybe low pass filter?
+        x_f = expo/100
+        x5_f = 1 - x_f
+        # # xleft = 0 if out_left == 0 else out_left / abs(out_left)
+        # # xright = 0 if out_right == 0 else out_right / abs(out_right)
+        # # vxmap11 = float(self.MAP(x, -32768, 32767, xleft, xright))
+        # fxmap = x_f * vxmap11 + x5_f * pow(vxmap11, 5)
+        # # return int(self.MAP(fxmap, -1, 1, -1000, 1000))
+        return int(self.MAP(x,-32768, 32767, out_left, out_right))
+
+    def apply_filter(self, x):
+        xfilt = self.livefilter(x)
+        return xfilt
+
     def read(self):
         global State
-        while not self.finish:
-            # r,w,x = select.select([self.pad], [], [])
-            select.select([self.pad], [], [])
-            for event in self.pad.read():
-                if event.type == ecodes.EV_KEY:
-                    categorized_event = categorize(event).event
-                    if categorized_event.code == 308:
-                        if categorized_event.value == 1:
-                            State = 255 if State == 0 else 0
-                            print(State)
-                if event.type == ecodes.EV_ABS:
-                    categorized_event = categorize(event).event
-                    if categorized_event.code == 0:
-                        RPYT['ROLL'] = categorized_event.value
-                    elif categorized_event.code == 1:
-                        RPYT['PITCH'] = categorized_event.value
-                    elif categorized_event.code == 3:
-                        RPYT['YAW'] = categorized_event.value
-                    elif categorized_event.code == 4:
-                        RPYT['THROTTLE'] = categorized_event.value
+        global finish_pad
+        global RPYT
+        while finish_pad == False:
+            try:
+                select.select([self.pad], [], [], 0.4)
+                for event in self.pad.read():
+                    if event.type == ecodes.EV_KEY:
+                        categorized_event = categorize(event).event
+                        if categorized_event.code == 308:
+                            if categorized_event.value == 1:
+                                State = 255 if State == 0 else 0
+                                print(State)
+                    if event.type == ecodes.EV_ABS:
+                        print("r:{}\tp:{}\ty:{}\tt:{}".format(RPYT["ROLL"], RPYT["PITCH"], RPYT["YAW"]
+                                                              , RPYT["THROTTLE"]))
+                        categorized_event = categorize(event).event
+                        if categorized_event.code == 0:
+                            RPYT['ROLL'] = self.apply_dummy_expo(categorized_event.value, 16, -500, 500)
+                        elif categorized_event.code == 1:
+                            RPYT['PITCH'] = self.apply_dummy_expo(categorized_event.value, 16, -500, 500)
+                        elif categorized_event.code == 3:
+                            RPYT['YAW'] = self.apply_dummy_expo(categorized_event.value, 10, -500, 500)
+                        elif categorized_event.code == 4:
+                            RPYT['THROTTLE'] = self.apply_dummy_expo(categorized_event.value, 38, 1000, 0)
+            except BlockingIOError:
+                continue
+            except OSError as e:
+                print("os error, message : {}, done with that".format(e.strerror))
+                break
+        time.sleep(2)
+        print("Finished reading pad")
 
     def cleanup(self):
+        global finish_pad
+        finish_pad = True
+        self.read_thread.join(2)
         self.pad.close()
-        self.finish = True
+
+        print("pad.cleanup()")
 
 
-parser = argparse.ArgumentParser(description='Issues a command for a receiver device, further doc TODO')
-parser.add_argument('-g', dest='gpio', type=int, default=17,
-                    help="GPIO pin (Default: 17)")
-parser.add_argument('-t', dest='protocol', type=int, default=None,
-                    help="Protocol (Default: 1)")
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Issues a command for a receiver device, further doc TODO')
+# parser.add_argument('-g', dest='gpio', type=int, default=17,
+#                     help="GPIO pin (Default: 17)")
+# parser.add_argument('-t', dest='protocol', type=int, default=None,
+#                     help="Protocol (Default: 1)")
+# args = parser.parse_args()
 finish_all = False
 commander = None
 e32 = None
@@ -280,12 +397,12 @@ pad = None
 
 
 def exit_handler(signal_, frame_):
-    global e32
-    global finish_all
-    global pad
-    e32.finish()
-    pad.cleanup()
-    finish_all = True
+    # global e32
+    # global finish_all
+    # global pad
+    # e32.finish()
+    # pad.cleanup()
+    # finish_all = True
     sys.exit(0)
 
 
--- test_uvc.py
+++ test_uvc.py
@@ -1,15 +1,18 @@
-import rc_command
+from uvc.uvc_bindings import CameraMode
+
+from rc_command import E32, PadDev
+from threading import Lock
 import threading
 import uvc
 import time
 import io
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import filedialog
 import sys
 import signal
 from PIL import ImageTk, Image
 
-tkimg = None
 finish_all = False
 
 
@@ -27,15 +30,18 @@ class FPVWindow:
         self.captured_frame = None
         self.uvc_device = None
         self.dev_list = []
+        self.e32 = None
+        self.pad = None
         self.xsize = 640 if xsize < 640 else xsize
         self.ysize = 480 if ysize < 480 else ysize
         self.rx_frame = rx_frame
+        self.finish_all = False
 
-        self.try_to_spawn_uvc_widget = threading.Thread(target=self.__try_to_spawn_uvc_widget)
         self.video_stream_thread = threading.Thread(target=self.__frame_to_tk)
         self.uvc_video_panel_update_thread = threading.Thread(target=self.__uvc_video_panel_update)
 
         self.root = tk.Tk()
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
         self.menu = tk.Menu(self.root)
         self.frame = tk.Frame(self.root, bg="white")
 
@@ -43,7 +49,7 @@ class FPVWindow:
         self.menu.add_cascade(label="Devices", menu=self.device_menu)
         self.device_menu.add_command(label="Choose receiver...", command=self.__try_start_receiver)
         self.device_menu.add_command(label="Choose controller...", command=self.__try_start_controller)
-
+        self.device_menu.add_command(label="Choose VTX...", command=self.__try_to_spawn_uvc_widget)
         self.widgets = {
             "UVC_VIDEO_PANEL": [tk.Label(self.root), 1, 3]
         }
@@ -57,84 +63,134 @@ class FPVWindow:
         # menu add
         self.root.config(menu=self.menu)
 
-        self.try_to_spawn_uvc_widget.start()
         self.root.title("FPV")
         self.root.geometry("{}x{}".format(self.xsize, self.ysize))
 
+    def on_closing(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.finish_all = True
+            if self.e32 is not None:
+                print("Closing rxtx")
+                self.e32.finish()
+                time.sleep(1)
+            if self.pad is not None:
+                print("Closing pad")
+                self.pad.cleanup()
+                time.sleep(1)
+            if self.video_stream_thread.is_alive():
+                print("Closing vid stream")
+                self.video_stream_thread.join(1)
+                time.sleep(1)
+            if self.uvc_video_panel_update_thread.is_alive():
+                print("Closing vid update panel")
+                self.uvc_video_panel_update_thread.join(1)
+                time.sleep(1)
+            print("Closing window")
+            self.root.destroy()
+
+
     def __try_to_spawn_uvc_widget(self):
+
+        vtxname = open_available_devices_list("VTX device", "/dev")
+        print(vtxname)
         while len(self.dev_list) == 0:
             print("trying to capture UVC device, first from the list will be taken!")
             time.sleep(1)
             self.dev_list = uvc.device_list()
 
-        print("found: ", self.dev_list[0]['uid'])
-
+        print("found: ", self.dev_list[0])
+        if self.capture_device is not None:
+            print("VTX already chosen")
+            return
         self.capture_device = uvc.Capture(self.dev_list[0]['uid'])
-        self.capture_device.frame_mode = (640, 480, 30)
+        if len(self.capture_device.available_modes) == 0:
+            print("No available UVC camera modes, returning")
+            return
+
+        print("Available UVC camera modes: ")
+        for mode in self.capture_device.available_modes:
+            print(f"\t {self.capture_device.name} mode: {mode}")
+            if mode.width == 640 and mode.height == 480 and mode.fps == 60:
+                self.capture_device.frame_mode = mode
+                print(f"\tChosen above one")
+
         self.capture_device.bandwidth_factor = 4
         self.video_stream_thread.start()
         self.uvc_video_panel_update_thread.start()
 
     def __uvc_video_panel_update(self):
-        self.widgets["UVC_VIDEO_PANEL"][0].imgtk = self.tkimg
-        self.widgets["UVC_VIDEO_PANEL"][0].configure(image=self.tkimg)
-        self.root.after(1, self.__uvc_video_panel_update)  # TODO cant be ran in a loop ?
+        if not self.finish_all:
+            self.widgets["UVC_VIDEO_PANEL"][0].imgtk = self.tkimg
+            self.widgets["UVC_VIDEO_PANEL"][0].configure(image=self.tkimg)
+            self.root.after(17, self.__uvc_video_panel_update)  # TODO cant be ran in a loop ?
 
     def __frame_to_tk(self):
-        while not finish_all:
+        while not self.finish_all:
             try:
                 self.captured_frame = self.capture_device.get_frame_robust()
                 self.tkimg = ImageTk.PhotoImage(Image.open(io.BytesIO(self.captured_frame.jpeg_buffer)))
-                time.sleep(0.001)
             except TypeError:
+                pass
+            except AttributeError:
                 pass
             except Exception:
                 pass
+            time.sleep(0.017)
 
     def __try_start_receiver(self):
         rxtxname = open_available_devices_list("Receiver device", "/dev")
         if rxtxname == "":
             print("Didn't choose any file, returning")
             return
-        self.e32 = rc_command.E32(rxtxname, 9600)
-        self.e32.write_settings("8N1", 115200, 6, 0, 1, 1)
+        if self.e32 is not None:
+            print("Receiver already chosen")
+            return
+        self.e32 = E32(rxtxname, 9600)
+        self.e32.write_settings("8N1", 38400, 6, 0, 1, 1)
         # self.e32.read_config()
         time.sleep(0.5)
         self.e32.start_reading()
 
     def __try_start_controller(self):
-        ctrlrname = open_available_devices_list("Controller device", "/dev/input")
-        if ctrlrname == "":
-            print("Didn't choose any file, returning")
-            return
-        self.pad = rc_command.PadDev(ctrlrname)
-        self.pad.start()
+        try:
+            ctrlrname = open_available_devices_list("Controller device", "/dev/input")
+            if ctrlrname == "":
+                print("Didn't choose any file, returning")
+                return
+            if self.pad is not None:
+                print("Controller already chosen")
+                return
+            self.pad = PadDev(ctrlrname)
+            self.pad.start()
+        except PermissionError:
+            print("Not enough permission to open file")
 
     def start(self):
         self.root.mainloop()
 
 
-def signal_handler(signal, frame):
-    finish_all = True
-    cap = None
-    # frame_thread.join()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
+# def signal_handler(signal, frame):
+#     global finish_all
+#     finish_all = True
+#     cap = None
+#     # frame_thread.join()
+#     sys.exit(0)
+#
+#
+# signal.signal(signal.SIGINT, signal_handler)
 
 
 def main():
     # global commander
-    global e32
-    global finish_all
-    global pad
+    # global e32
+    # global finish_all
+    # global pad
     main_window = FPVWindow(xsize=640, ysize=480, rx_frame=None)
     main_window.prepare_window()
     main_window.start()
 
-    while not finish_all:
-        time.sleep(0.05)
+    # while not finish_all:
+    #     time.sleep(0.05)
 
 
 if __name__ == '__main__':
